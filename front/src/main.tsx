5871ae484716228e0d1b7feda58172a06c061005
Gonzalo <villalbagonzalo154@gmail.com>
2024-10-09 11:34:13 -0300

    CDN tailwind QUEDO PILA

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import Prueba from '../src/components/Expediente.tsx'
 import ExpedientesCRUD from '../src/components/Expediente.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
