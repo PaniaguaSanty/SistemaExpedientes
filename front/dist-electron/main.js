5f7813f0954510f64eed87d7d5b55c0c0028fedc
Santiago Agustín Bosquet <paniaguasanty10@gmail.com>
2024-10-30 10:18:10 -0300

    Full screen añadida

--- front/dist-electron/main.js
+++ front/dist-electron/main.js
@@ -12,6 +12,8 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win;
 function createWindow() {
   win = new BrowserWindow({
+    fullscreen: true,
+    // Esto hace que la ventana se abra en pantalla completa
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs")
--- front/electron/main.ts
+++ front/electron/main.ts
@@ -28,6 +28,8 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
+    fullscreenable: true,  // Permite cambiar a pantalla completa
+    maximized: true,       // Abre la ventana maximizada
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
